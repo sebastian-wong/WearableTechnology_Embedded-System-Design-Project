1cfb47c564b44a383390a1ecc876cec118b573a4
Sebastian Wong <sebastian.wong@hotmail.com>
2014-10-10 16:46:09 +0800

    Tested AudioFeedback class
    Tested module by making class give audio feed back continuously

--- Raspberrypi/UserInput/espeak_cls.py
+++ Raspberrypi/UserInput/espeak_cls.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 import subprocess
+import threading
 
 class AudioFeedback(object):
 	def __init__ (self):
 		self._process = None
 
-	def Feedback(self,text):
-		#Speaking with amplitude=200, pitch=50, speed=150, capital=20, language= english-rp, voice=female2
-		#self._process = subprocess.Popen(['espeak -a200 -p50 -s150 -k20 -ven-rp+f2', text])
-		self._process = subprocess.Popen(['espeak', text])
-		#self._process.communicate()		
-		self._process.terminate()
-		self._process.poll();
-		#normal return statement should suffice
-		#return	self._process.returncode
+	def Feedback(self,text,timeout=None):
 
-#to be executed when called by main program
-if __name__ == '__main__':
+		#Speaking with amplitude=200, pitch=50, speed=150, capital=20, language= english-rp, voice=female2
+		self._process = subprocess.Popen(['espeak', '-a200', '-p50', '-s150', '-k20','-ven-rp+f2', text])
+		#wait for subprocess to terminate
+		self._process.wait()
+		
+		
+
+#testing AudioFeedback class with python
+if __name__ == "__main__":
 	text = "hello world"
 	output = AudioFeedback()
 	output.Feedback(text)
--- Raspberrypi/UserInput/espeak_test.py
+++ Raspberrypi/UserInput/espeak_test.py
@@ -1,7 +1,38 @@
 import os
+import time
 
 from espeak_cls import AudioFeedback
 
 output = AudioFeedback()
-string = "hello world"
+string = "Hello Chew Yi Xiu "
 output.Feedback(string)
+time.sleep(2)
+
+
+while True:		
+	string = "Yanqing likes someone"
+	output.Feedback(string)
+	time.sleep(1)
+
+
+	string = "Angela has tin tin"
+	output.Feedback(string)
+	time.sleep(1)
+
+
+	string = "It's bob the builder"
+	output.Feedback(string)
+	time.sleep(1)
+
+
+	string = "Can we fix it?"
+	output.Feedback(string)
+	time.sleep(1)
+
+	string = "It's bob the builder"
+	output.Feedback(string)
+	time.sleep(1)
+
+	string = "YES WE CAN!"
+	output.Feedback(string)
+	time.sleep(1)
